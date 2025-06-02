14550959844bb8715ffeffb7e09f338e87002345
penguinlord-2701 <147300789+penguinlord-2701@users.noreply.github.com>
2025-06-02 13:11:25 +0000

    variable coomit

---
+++ basic/variable.js
@@ -0,0 +1,19 @@
+const accountId = 144563
+let accountEmail ="daksh@gmail.com"
+var accountpassword ="12345"
+accountCity ="Jaipur"
+
+//saccountId =2 // not allowed
+
+accountEmail ="hacha@gmail.com"
+accountpassword="646465"
+accountCity="mumbai"
+let accountState ;
+
+/*
+prefer not to use var in blockscope and functional scope
+
+*/
+console.log(accountId);
+
+console.log([accountId,accountEmail,accountpassword,accountCity,accountState])
