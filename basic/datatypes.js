1f4eb12fed1db0d1a93ac78e882e1bc2514fc544
penguinlord-2701 <147300789+penguinlord-2701@users.noreply.github.com>
2025-06-02 13:20:19 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using node.js and not browser
+
+console.log(3
+    +3)//code readability is must 
+console.log("daksh")
+ //tc39.es and mdn 
+
+ let name="daksh"
+ let age =20
+ let isloggedon= false
+
+ // number -> 2 to power 3
+ // biginit
+ // boolean => true/false
+ // null=> standalone value 
+ // undefined => yet to assign value
+// symbol => uniqueness 
+
+// object |
+
+console.log(typeof age);
