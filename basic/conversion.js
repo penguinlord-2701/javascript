f3d33de8270dd6af4ffb05ce9ea1bbd9fade0b01
penguinlord-2701 <147300789+penguinlord-2701@users.noreply.github.com>
2025-06-04 11:44:52 +0000

    conversion_datatypes

---
+++ basic/conversion.js
@@ -0,0 +1,29 @@
+let score = "daksh"
+
+//console.log(typeof score );
+//console.log(typeof(score)); // written as method
+
+// number is written in lower  case (terminal output)
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" -> 33
+//null -> 0
+//true = 1 or false =0
+// "33abc"=> NaN  Not a number 
+
+//let isLoggedIn= "daksh"
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)
+
+//"" => false
+//"daksh" => true
+// 1 -> true & 0-> false
+
+let somenumber =33
+let stringnumber = String(somenumber);
+console.log(stringnumber)
+console.log(typeof stringnumber)
