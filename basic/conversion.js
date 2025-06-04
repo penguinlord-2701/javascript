06fa23ef57fb9c17f6048dac72ebcfcdfcab9368
penguinlord-2701 <147300789+penguinlord-2701@users.noreply.github.com>
2025-06-04 12:01:16 +0000

    operations

--- basic/conversion.js
+++ basic/conversion.js
@@ -23,7 +23,33 @@ let valueInNumber = Number(score)
 //"daksh" => true
 // 1 -> true & 0-> false
 
-let somenumber =33
-let stringnumber = String(somenumber);
-console.log(stringnumber)
-console.log(typeof stringnumber)
+//let somenumber =33
+//let stringnumber = String(somenumber);
+//console.log(stringnumber)
+//console.log(typeof stringnumber)
+
+//******************Operations****************** */
+
+let value =3 
+let negvalue = -value
+console.log(negvalue);
+
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = "dax"
+let str3 =str1 + str2 
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2 + "2");
+
+console.log(+true)
+
+let num1,num2,num3;
+num1=num2=num3=2+2  // not advisable
+let gamecounter =100
+++gamecounter;
+console.log(gamecounter);
