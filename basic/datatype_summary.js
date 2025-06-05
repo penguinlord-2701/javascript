c07c24e0d7bd5969df9dd2b729b64b22d36e1ebc
penguinlord-2701 <147300789+penguinlord-2701@users.noreply.github.com>
2025-06-05 16:03:50 +0000

    summary

---
+++ basic/datatype_summary.js
@@ -0,0 +1,27 @@
+
+// primitive 
+
+
+//7 types : strings ,numbers , boolean , null , undefiend , symbol
+//BigInt
+
+
+const score = 100 
+
+const scorevalue=100.3
+
+const isLoggedIn= false
+
+const id = Symbol('123')
+const id1 = Symbol('123')
+
+console.log(id === id1);
+
+const bigNumber = 3456879521455545445;
+console.log(typeof(bigNumber));
+
+//refrences (non primitive)
+
+//arrray , objects, functions 
+
+
