588e3c21ce39a73864c200e0199073d4a4ecc8db
penguinlord-2701 <147300789+penguinlord-2701@users.noreply.github.com>
2025-06-05 12:32:54 +0000

    comparision

---
+++ basic/comparision.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null >= 0) // null is converted to number and then does comparison
+
+// === strict check  => check data type also 
+ console.log("2"=== 2) // returns false value 
+
+ // use clean code because that is given more priority 
+
+ //  > < >= <=  && >! !=  this are normal comparision
+
