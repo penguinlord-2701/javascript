06ec22b0f292f8e0165ea3ea4ebd9d804d14827e
penguinlord-2701 <147300789+penguinlord-2701@users.noreply.github.com>
2025-06-09 14:29:22 +0000

    number done

---
+++ basic/number.js
@@ -0,0 +1,33 @@
+const score = 400
+const balance =new Number(100)
+//console.log(balance)
+//console.log(score)
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const on= 123.456
+//console.log(on.toPrecision(4)) //  prcise value  upto rounding off the value accordingly
+
+const hundreds =1000000
+//console.log(hundreds.toLocaleString('en-IN'))// follows basic unit standard
+
+//+++++++++++++MATHS+++++++++++++++++++++++++++//
+
+console.log(Math);
+console.group(Math.abs(-4));
+console.log(Math.round(4.3));
+// ceil => top value
+// floor => lower value
+//sqrt
+//math.min
+//math.max 
+
+//.random provide value between 0  and 1 
+//console.log(Math.random()*10);
+
+const min =10
+const max =20 
+
+console.log(Math.floor(Math.random()*(max-min+1)+min)); // generalize formula to count numaber in range 
+
+
